1568ace3fb81fd857bdc7ffb60ff71dbcfd3d1f2
Evaluator <thrvoid97@gmail.com>
2021-06-30 18:01:39 +0300

    Ship placement system added, Grid system changed

---
+++ Assets/Scripts/CheckPlacement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CheckPlacement : MonoBehaviour
+{
+    [SerializeField] private Material GreenMat, RedMat;
+    public bool canPlace = true;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Compartment") || other.CompareTag("Obstacle"))
+        {
+            GetComponent<MeshRenderer>().material = GreenMat;
+            transform.GetChild(0).GetComponent<MeshRenderer>().material = RedMat;
+            canPlace = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Compartment") || other.CompareTag("Obstacle"))
+        {
+            GetComponent<MeshRenderer>().material = RedMat;
+            transform.GetChild(0).GetComponent<MeshRenderer>().material = GreenMat;
+            canPlace = true;
+        }
+    }
+
+}
--- Assets/Scripts/GridStats.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class GridStats : MonoBehaviour
-{
-    public int visited = -1;
-    public int positionX = 0;
-    public int positionY = 0;
-}
